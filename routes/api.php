483c5ba49e68a46d0ed1f0e8f785988544553909
Abdukhaligov <hikmat.pou@gmail.com>
2024-08-15 02:14:09 +0400

    improve: Notification server / add NotificationClient

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,23 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Shared\Clients\OtpClient;
+use Shared\Notification\Channel;
+use Shared\Notification\Provider;
+use Shared\NotificationClient;
 
 Route::middleware('auth.server')->get('', function () {
     return Auth::guard('server')->user();
 });
 
 Route::get('/otp-generate', function () {
-    $otp = \Shared\OtpClient::generateOtp();
+    $otp = OtpClient::generateOtp();
+
+    if ($otp->success) {
+        $message = 'OTP: ' . $otp->code;
+
+        NotificationClient::sendMessage(Provider::TELEGRAM, Channel::OTP, $message);
+    }
 
     return response()->json($otp);
 });
