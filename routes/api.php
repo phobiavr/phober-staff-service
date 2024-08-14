9fbdd027a5ce122b33302e177870b355e86cc904
Abdukhaligov <hikmat.pou@gmail.com>
2024-08-15 01:30:27 +0400

    add: OTP middleware

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth.server')->get('', function () {
     return Auth::guard('server')->user();
 });
+
+Route::get('/otp-generate', function () {
+    $otp = \Shared\OtpClient::generateOtp();
+
+    return response()->json($otp);
+});
+
+Route::middleware('otp')->group(function () {
+    Route::get('/otp-pass', function () {
+        return true;
+    });
+});
+
