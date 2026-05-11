9ccf062f1e0e9cee513461fa509bdb3947d3c755
Hikmat Abdukhaligov <hikmat.abdukhaligov@americor.com>
2026-05-11 11:55:05 +0400

    refactor: events

--- app/Events/SessionChanged.php
+++ app/Events/SessionChanged.php
@@ -4,11 +4,11 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SessionChanged implements ShouldBroadcastNow
+class SessionChanged implements ShouldBroadcast
 {
     use Dispatchable;
     use InteractsWithSockets;
