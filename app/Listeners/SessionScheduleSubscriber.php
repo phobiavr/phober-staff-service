0cf0ffaccf97797bf2e58cf5b3af2667bd63db65
Hikmat Abdukhaligov <hikmat.abdukhaligov@americor.com>
2026-06-07 00:09:49 +0400

    add: cancel active session & fix canceled queue

--- app/Listeners/SessionScheduleSubscriber.php
+++ app/Listeners/SessionScheduleSubscriber.php
@@ -9,29 +9,24 @@ use App\Events\SessionStarted;
 use Illuminate\Events\Dispatcher;
 use Phobiavr\PhoberLaravelCommon\Jobs\HandleSessionSchedule;
 
-class SessionScheduleSubscriber
-{
-    public function handleCanceled(SessionCanceled $event): void
-    {
+class SessionScheduleSubscriber {
+    public function handleCanceled(SessionCanceled $event): void {
         HandleSessionSchedule::dispatch($event->session->instance_id, 'cancel')->onQueue('device');
     }
 
-    public function handleStarted(SessionStarted $event): void
-    {
+    public function handleStarted(SessionStarted $event): void {
         HandleSessionSchedule::dispatch($event->session->instance_id, 'start', $event->session->time)->onQueue('device');
     }
 
-    public function handleFinished(SessionFinished $event): void
-    {
+    public function handleFinished(SessionFinished $event): void {
         HandleSessionSchedule::dispatch($event->session->instance_id, 'finish')->onQueue('device');
     }
 
-    public function subscribe(Dispatcher $events): void
-    {
+    public function subscribe(Dispatcher $events): void {
         $events->listen(SessionCreated::class, SessionCreatedListener::class);
 
         $events->listen(SessionStarted::class, [self::class, 'handleStarted']);
         $events->listen(SessionFinished::class, [self::class, 'handleFinished']);
-        $events->listen(SessionFinished::class, [self::class, 'handleFinished']);
+        $events->listen(SessionCanceled::class, [self::class, 'handleCanceled']);
     }
 }
