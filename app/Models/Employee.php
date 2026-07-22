abe885bcb82be1fdd013492ade3cc56ac1912c9d
Hikmat Abdukhaligov <hikmat.abdukhaligov@americor.com>
2026-07-22 04:51:07 +0400

    fix: employee sessions query

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,72 +6,58 @@ use Illuminate\Database\Eloquent\Model;
 use Phobiavr\PhoberLaravelCommon\Enums\SessionStatusEnum;
 
 class Employee extends Model {
-    protected $with = ['sessions'];
-
     public function sessions() {
         return $this->hasMany(Session::class, 'serviced_by');
     }
 
     private function activeSessions() {
-        return $this->sessions->whereIn('status', [
+        return $this->sessions()->whereIn('status', [
             SessionStatusEnum::ACTIVE->value,
             SessionStatusEnum::FINISHED->value,
         ]);
     }
 
-    public function getServicedTotalAttribute() {
+    public function getServicedTotalAttribute(): int {
         return $this->activeSessions()->count();
     }
 
-    public function getServicedMinutesTotalAttribute() {
-        return $this->activeSessions()->sum('time');
+    public function getServicedMinutesTotalAttribute(): int {
+        return (int) $this->activeSessions()->sum('time');
     }
 
-    public function getServicedInADayAttribute() {
+    public function getServicedInADayAttribute(): int {
         return $this->activeSessions()
-            ->filter(function ($session) {
-                return $session->created_at->isToday();
-            })
+            ->whereDate('created_at', now())
             ->count();
     }
 
-    public function getServicedMinutesInADayAttribute() {
-        return $this->activeSessions()
-            ->filter(function ($session) {
-                return $session->created_at->isToday();
-            })
+    public function getServicedMinutesInADayAttribute(): int {
+        return (int) $this->activeSessions()
+            ->whereDate('created_at', now())
             ->sum('time');
     }
 
-    public function getServicedInAWeekAttribute() {
+    public function getServicedInAWeekAttribute(): int {
         return $this->activeSessions()
-            ->filter(function ($session) {
-                return $session->created_at->between(now()->startOfWeek(), now()->endOfWeek());
-            })
+            ->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])
             ->count();
     }
 
-    public function getServicedMinutesInAWeekAttribute() {
-        return $this->activeSessions()
-            ->filter(function ($session) {
-                return $session->created_at->between(now()->startOfWeek(), now()->endOfWeek());
-            })
+    public function getServicedMinutesInAWeekAttribute(): int {
+        return (int) $this->activeSessions()
+            ->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])
             ->sum('time');
     }
 
-    public function getServicedInAMonthAttribute() {
+    public function getServicedInAMonthAttribute(): int {
         return $this->activeSessions()
-            ->filter(function ($session) {
-                return $session->created_at->month == now()->month;
-            })
+            ->whereBetween('created_at', [now()->startOfMonth(), now()->endOfMonth()])
             ->count();
     }
 
-    public function getServicedMinutesInAMonthAttribute() {
-        return $this->activeSessions()
-            ->filter(function ($session) {
-                return $session->created_at->month == now()->month;
-            })
+    public function getServicedMinutesInAMonthAttribute(): int {
+        return (int) $this->activeSessions()
+            ->whereBetween('created_at', [now()->startOfMonth(), now()->endOfMonth()])
             ->sum('time');
     }
 
